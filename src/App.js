87ab66b93d276daa9709d58b06e890f37ef2e3ef
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-07 14:17:31 +0100

    Add google map to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './LandingPage';
+import MapSection from './map'
+
+const location = {
+  address: '10, Market Street, Somolu, Lagos State.',
+  lat: 6.533805520242892,
+  lng: 3.375872797362381, 
+}
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <MapSection location={location} zoomLevel={17} />
     </div>
   );
 }
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -4,8 +4,14 @@ import "./LandingPage.css";
 function LandingPage() {
   return (
     <div className="landing-container">
-      <h1>Welcome to My Website</h1>
-      <p>This is a simple landing page created using create-react-app.</p>
+      <h1>Welcome to My GoMyCode Store</h1>
+      <p>
+        This is a simple landing page created using create-react-app. Visit our
+        store today!
+      </p>
+      <p>
+        Store location below...
+      </p>
       <button>Learn More</button>
     </div>
   );
