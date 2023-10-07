6b1c7c3d15ff54040110ecb6cc489a9ca5db5412
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-07 13:13:48 +0100

    innitialize git

---
+++ src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <h1>Welcome to My Website</h1>
+      <p>This is a simple landing page created using create-react-app.</p>
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default LandingPage;
